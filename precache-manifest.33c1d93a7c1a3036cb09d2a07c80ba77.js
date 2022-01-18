716878d9f5d8639aaaa5bf3bbe4e638026749881
khanatifk991 <91177723+khanatifk991@users.noreply.github.com>
2022-01-19 00:34:32 +0530

    Updates

---
+++ precache-manifest.33c1d93a7c1a3036cb09d2a07c80ba77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c341e779897d32f2c21e576d6d36839",
+    "url": "/myTextutils/index.html"
+  },
+  {
+    "revision": "927324aeb3d4c97f8107",
+    "url": "/myTextutils/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9d858bb8ee2878a64417",
+    "url": "/myTextutils/static/js/2.3a02ade8.chunk.js"
+  },
+  {
+    "revision": "4d8b8910afb7a93ea449",
+    "url": "/myTextutils/static/js/3.fe76c5f2.chunk.js"
+  },
+  {
+    "revision": "927324aeb3d4c97f8107",
+    "url": "/myTextutils/static/js/main.86ed500d.chunk.js"
+  },
+  {
+    "revision": "a7dc3947f0ca432b2769",
+    "url": "/myTextutils/static/js/runtime~main.8e40d5bc.js"
+  }
+]);
